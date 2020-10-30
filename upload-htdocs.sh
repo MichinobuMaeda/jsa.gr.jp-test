cec8f07d2e07f596adff541e7a12ca76da92c45d
Michinobu Maeda <michinobumaeda@gmail.com>
2020-10-30 20:26:37 +0900

    202011

--- upload-htdocs.sh
+++ upload-htdocs.sh
@@ -1,4 +1,4 @@
-scp htdocs/04pub/2020/202010JJStokusyu.pdf jsa.gr.jp:~/htdocs/04pub/2020/
+scp htdocs/04pub/2020/202011JJStokusyu.pdf jsa.gr.jp:~/htdocs/04pub/2020/
 scp htdocs/04pub/0401jjs/2020contents.html jsa.gr.jp:~/htdocs/04pub/0401jjs/
 scp htdocs/04pub/index.html jsa.gr.jp:~/htdocs/04pub/
 scp htdocs/jjs-cover-s.jpg jsa.gr.jp:~/htdocs/
