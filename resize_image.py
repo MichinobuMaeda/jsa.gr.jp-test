a1267e8283e38481e94a2af5abe6540ddc0a3ed7
Michinobu Maeda <michinobumaeda@gmail.com>
2021-01-06 21:08:38 +0900

    2021-01

--- resize_image.py
+++ resize_image.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import sys
 from PIL import Image
 
--- upload-htdocs.sh
+++ upload-htdocs.sh
@@ -1,5 +1,5 @@
-scp htdocs/04pub/2020/202012JJStokusyu.pdf jsa.gr.jp:~/htdocs/04pub/2020/
-scp htdocs/04pub/0401jjs/2020contents.html jsa.gr.jp:~/htdocs/04pub/0401jjs/
+scp htdocs/04pub/2021/202101JJStokusyu.pdf jsa.gr.jp:~/htdocs/04pub/2021/
+scp htdocs/04pub/0401jjs/2021contents.html jsa.gr.jp:~/htdocs/04pub/0401jjs/
 scp htdocs/04pub/index.html jsa.gr.jp:~/htdocs/04pub/
 scp htdocs/jjs-cover-s.jpg jsa.gr.jp:~/htdocs/
 scp htdocs/index.html jsa.gr.jp:~/htdocs/
