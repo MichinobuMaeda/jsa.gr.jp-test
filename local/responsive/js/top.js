c66bd20b8a14346b9cbc68c8db59688ed86411b9
Michinobu Maeda <michinobumaeda@gmail.com>
2021-01-24 17:13:11 +0900

    move from atw to sakura

--- local/responsive/js/top.js
+++
@@ -1,9 +0,0 @@
-// 指定した年月日の 7日後まで New を表示する。
-function writeNew(y, m, d) {
-  var modDate = new Date(y, m-1, d);
-  var nowDate = new Date();
-  var newDays = 7;
-  if ((nowDate-modDate)/(1000*60*60*24) < 7) {
-    document.write('<span class="label label-warning">New</span>');
-  }
-}
