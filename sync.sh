1854851240044ac68dc1992c4298e3f8a770514e
Michinobu Maeda <michinobumaeda@gmail.com>
2021-01-24 16:16:15 +0900

    move from atw to sakura

--- sync.sh
+++ sync.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rsync -av jsa.gr.jp:/home/chasu/htdocs/* ./htdocs/
-cp robots.txt htdocs/
+rsync -av jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku/
--- upload-htdocs.sh
+++ upload-htdocs.sh
@@ -1,5 +1,5 @@
-scp htdocs/04pub/2021/202101JJStokusyu.pdf jsa.gr.jp:~/htdocs/04pub/2021/
-scp htdocs/04pub/0401jjs/2021contents.html jsa.gr.jp:~/htdocs/04pub/0401jjs/
-scp htdocs/04pub/index.html jsa.gr.jp:~/htdocs/04pub/
-scp htdocs/jjs-cover-s.jpg jsa.gr.jp:~/htdocs/
-scp htdocs/index.html jsa.gr.jp:~/htdocs/
+scp www/zenkoku/04pub/2021/202101JJStokusyu.pdf jsa.gr.jp:~/www/zenkoku/04pub/2021/
+scp www/zenkoku/04pub/0401jjs/2021contents.html jsa.gr.jp:~/www/zenkoku/04pub/0401jjs/
+scp www/zenkoku/04pub/index.html jsa.gr.jp:~/www/zenkoku/04pub/
+scp www/zenkoku/jjs-cover-s.jpg jsa.gr.jp:~/www/zenkoku/
+scp www/zenkoku/index.html jsa.gr.jp:~/www/zenkoku/
