29970e6cb16d881b2a9e968abf61ee1d6e702926
Michinobu Maeda <michinobumaeda@gmail.com>
2021-02-26 19:48:17 +0900

    2021-03

--- upload-htdocs.sh
+++ upload-htdocs.sh
@@ -1,4 +1,4 @@
-scp www/zenkoku/04pub/2021/202102JJStokusyu.pdf jsa.gr.jp:~/www/zenkoku/04pub/2021/
+scp www/zenkoku/04pub/2021/202103JJStokusyu.pdf jsa.gr.jp:~/www/zenkoku/04pub/2021/
 scp www/zenkoku/04pub/0401jjs/2021contents.html jsa.gr.jp:~/www/zenkoku/04pub/0401jjs/
 scp www/zenkoku/04pub/index.html jsa.gr.jp:~/www/zenkoku/04pub/
 scp www/zenkoku/jjs-cover-s.jpg jsa.gr.jp:~/www/zenkoku/
