8aee787a4130d433f6aa118755108b8684d5e38f
Michinobu Maeda <michinobumaeda@gmail.com>
2021-03-29 03:48:14 +0900

    for windows

---
+++ jjs-upload.bat
@@ -0,0 +1,5 @@
+upload.bat www/zenkoku/04pub/2021/202103JJStokusyu.pdf
+upload.bat www/zenkoku/04pub/0401jjs/2021contents.html
+upload.bat www/zenkoku/04pub/index.html
+upload.bat www/zenkoku/jjs-cover-s.jpg
+upload.bat www/zenkoku/index.html
