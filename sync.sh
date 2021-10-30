acf9090fbb1ae21a60076574287b62126e84df9c
Michinobu Maeda <michinobumaeda@gmail.com>
2021-10-30 17:44:24 +0900

    update template

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsync -av jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku/
+rsync -av --delete jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku/
+rsync -av --delete jsa.gr.jp:/home/jsazenkoku/OLD/* ./OLD/
