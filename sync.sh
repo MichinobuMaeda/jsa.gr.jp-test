53c27c8b18a87d607708faa49fd5cff068b404e7
Michinobu Maeda <michinobumaeda@gmail.com>
2021-11-06 23:04:47 +0900

    add comment

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rsync -av --delete jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku/
 rsync -av --delete jsa.gr.jp:/home/jsazenkoku/OLD/* ./OLD/
+rsync -av --delete jsa.gr.jp:/home/jsazenkoku/data/* ./data/
+rsync -av jsa.gr.jp:/home/jsazenkoku/log/* ./log/
