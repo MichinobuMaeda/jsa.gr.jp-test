1c849faed19470d0030ee00e364e949bc3525dab
Michinobu Maeda <michinobumaeda@gmail.com>
2021-12-12 18:03:50 +0900

    add sync dirs

--- sync.bat
+++ sync.bat
@@ -1,1 +1,5 @@
-rsync -av -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku
+rsync -av --delete -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku
+rsync -av --delete -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@ jsa.gr.jp:/home/jsazenkoku/OLD/* ./OLD/
+rsync -av --delete -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@ jsa.gr.jp:/home/jsazenkoku/data/* ./data/
+rsync -av          -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@ jsa.gr.jp:/home/jsazenkoku/log/* ./log/
+rsync -av --delete -e "ssh -F %USERPROFILE%\.ssh\config" jsazenkoku@ jsa.gr.jp:/home/jsazenkoku/linkchecker/* ./linkchecker/
--- sync.sh
+++ sync.sh
@@ -3,3 +3,4 @@ rsync -av --delete jsa.gr.jp:/home/jsazenkoku/www/zenkoku/* ./www/zenkoku/
 rsync -av --delete jsa.gr.jp:/home/jsazenkoku/OLD/* ./OLD/
 rsync -av --delete jsa.gr.jp:/home/jsazenkoku/data/* ./data/
 rsync -av jsa.gr.jp:/home/jsazenkoku/log/* ./log/
+rsync -av jsa.gr.jp:/home/jsazenkoku/linkchecker/* ./linkchecker/
