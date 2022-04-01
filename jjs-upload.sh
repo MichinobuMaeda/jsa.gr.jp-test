2bbe86fe449982d5d91593edec1ce521edfd61fd
Michinobu Maeda <michinobumaeda@gmail.com>
2022-04-02 02:09:01 +0900

    2022-04

--- jjs-upload.sh
+++ jjs-upload.sh
@@ -1,4 +1,4 @@
-./upload.sh www/zenkoku/04pub/2022/202203JJStokusyu.pdf
+./upload.sh www/zenkoku/04pub/2022/202204JJStokusyu.pdf
 ./upload.sh www/zenkoku/04pub/0401jjs/2022contents.html
 ./upload.sh www/zenkoku/04pub/index.html
 ./upload.sh www/zenkoku/jjs-cover-s.jpg
